6029147e7bab6f09ee25d0433c0785eab808d1a5
Dido <dido_nikolov01@abv.bg>
2022-03-12 08:50:15 +0200

    components

---
+++ Angular/2.Components/lab/articles-app/src/app/models/article.model.ts
@@ -0,0 +1,8 @@
+export class Article {
+    constructor(public title: string, public description: string, public author:string, public imageUrl: string) {
+        this.title = title;
+        this.description = description;
+        this.author = author;
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ Angular/2.Components/test-project/src/app/domain/game.ts
@@ -0,0 +1,8 @@
+export class Game {
+    
+    constructor(public id: number, public title: string, public image: string) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+    }
+}
