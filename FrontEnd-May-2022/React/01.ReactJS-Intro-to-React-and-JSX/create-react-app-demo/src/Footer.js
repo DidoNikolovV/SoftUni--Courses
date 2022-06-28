aaea4fc1858575d17799346a863ff0a96efa978c
Dido <dido_nikolov01@abv.bg>
2022-06-28 20:49:48 +0300

    01.Intro-to-React-&-JSX

---
+++ FrontEnd-May-2022/React/01.ReactJS-Intro-to-React-and-JSX/create-react-app-demo/src/Footer.js
@@ -0,0 +1,13 @@
+function Footer() {
+    return <div>All rights reserved &copy;</div>;
+}
+
+// function Footer() {
+//     return React.createElement(
+//         'div',
+//         {},
+//         'All rights reserved &copy;'
+//     );
+// }
+
+export default Footer;
