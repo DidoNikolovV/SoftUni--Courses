10988cda7b1a65e8219a3c539d329b107b0980b1
Dido <dido_nikolov01@abv.bg>
2022-07-06 16:37:03 +0300

    Components Deep Dive lecture resources

---
+++ FrontEnd-May-2022/React/03.ReactJS-Components-Deep-Dive/deep-dive-demo/src/components/Book.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+import styles from './Book.module.css';
+
+
+export const Book = (props) => {
+    const [highlighted, setHighlighted] = useState(false);
+    const [deleted, setDeleted] = useState(false);
+
+    useEffect(() => {
+        console.log('Mounting ' + props.title);
+    }, []);
+
+
+    useEffect(() => {
+        console.log('Updating ' + props.title);
+    }, [highlighted, props.title]);
+
+    const clickHandler = () => {
+        setHighlighted(state => !state);
+    };
+
+    const deleteHandler = () => {
+        setDeleted(true);
+    };
+
+    let style = {};
+    if (highlighted) {
+        style.backgroundColor = 'blue';
+    }
+
+    if (deleted) {
+        return <h2>Deleted</h2>;
+    }
+
+    return (
+        <li style={style} className={styles['book-item']}>
+            <article>
+                <h2>{props.title}</h2>
+                <div>Price: {props.price}</div>
+                <div>Year: {props.year}</div>
+                <footer>
+                    <button onClick={clickHandler}>Highlight</button>
+                    <button onClick={deleteHandler}>Delete</button>
+                    <span className={styles.author}>Author: {props.author}</span>
+                </footer>
+            </article>
+        </li>
+    );
+};
