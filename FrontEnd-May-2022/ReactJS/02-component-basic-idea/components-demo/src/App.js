2edf7990efeac69e77b50b421cbb90f9b8eb7499
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-09-01 09:06:29 +0300

    Add useState

---
+++ FrontEnd-May-2022/ReactJS/02-component-basic-idea/components-demo/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { Header } from './components/Header';
+import { BookList } from './components/BookList';
+import { Timer } from './components/Timer';
+
+function App() {
+
+	const books = [
+		{ "title": "Northanger Abbey", "author": "Austen, Jane", "year": 1814, "edition": "Penguin", "price": 18.2 },
+		{ "title": "War and Peace", "author": "Tolstoy, Leo", "year": 1865, "edition": "Penguin", "price": 12.7 },
+		{ "title": "Anna Karenina", "author": "Tolstoy, Leo", "year": 1875, "edition": "Penguin", "price": 13.5 },
+		{ "title": "Mrs. Dalloway", "author": "Woolf, Virginia", "year": 1925, "edition": "Harcourt Brace", "price": 25 },
+		{ "title": "The Hours", "author": "Cunnningham, Michael", "year": 1999, "edition": "Harcourt Brace", "price": 12.35 },
+	];
+
+	return (
+		<div className="App">
+			<Header>
+				<span class="fancy-font">Book</span> Library
+			</Header>
+			<Timer />
+			<BookList books={books}></BookList>
+
+		</div>
+	);
+}
+
+export default App;
---
+++ FrontEnd-May-2022/ReactJS/02-component-basic-idea/components-demo/src/components/Timer.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+export const Timer = (props) => {
+    const [time, setTime] = useState(0);
+    setTimeout(() => {
+        setTime(time + 1);
+    }, 1000);
+
+    return (
+        <div>
+            <h2>Timer: {time} sec.</h2>
+        </div>
+    );
+};
