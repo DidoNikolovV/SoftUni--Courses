64072bfc4d7e7f0f1638502deb3afdfe76783880
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-09-02 09:54:30 +0300

    Refactor the way of rendering books

--- FrontEnd-May-2022/ReactJS/03-components-deep-dive/src/components/BookList.js
+++ FrontEnd-May-2022/ReactJS/03-components-deep-dive/src/components/BookList.js
@@ -1,24 +1,9 @@
+import { Book } from "./Book";
 
 export const BookList = (props) => {
-
-    const bookElements = props.books.map(book => (
-        <li>
-            <article>
-                <h2>{book.title}</h2>
-                <div>Price: {book.price}</div>
-                <div>Year: {book.year}</div>
-                <footer>
-                    <span>Author: {book.author}</span>
-                </footer>
-            </article>
-        </li>
-    ));
-
-
-
     return (
         <ul>
-            {bookElements}
+            {props.books.map(book => <Book {...book} />)}
         </ul>
     );
 };
