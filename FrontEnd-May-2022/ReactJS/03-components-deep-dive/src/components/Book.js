922768aeef4e876963b7ebcc63f6f4bdb007ae5e
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-09-03 13:33:34 +0300

    Add useEffect

--- FrontEnd-May-2022/ReactJS/03-components-deep-dive/src/components/Book.js
+++ FrontEnd-May-2022/ReactJS/03-components-deep-dive/src/components/Book.js
@@ -1,13 +1,47 @@
+import { useState, useEffect } from "react";
 
 export const Book = (props) => {
-    return (<li>
-        <article>
-            <h2>{props.title}</h2>
-            <div>Price: {props.price}</div>
-            <div>Year: {props.year}</div>
-            <footer>
-                <span>Author: {props.author}</span>
-            </footer>
-        </article>
-    </li>);
+    const [highlighted, setHighlighted] = useState(false);
+    const [marked, setMarked] = useState(false);
+
+    useEffect(() => {
+        console.log('Mounting: ' + props.title);
+
+    }, []);
+
+    useEffect(() => {
+        console.log('Updating: ' + props.title);
+    }, [highlighted, marked]);
+
+    const clickHandler = () => {
+        setHighlighted(state => !state);
+    };
+
+    const deleteHandler = () => {
+        setMarked(true);
+    };
+
+    let style = {};
+    if (highlighted) {
+        style.backgroundColor = 'green';
+    }
+
+    if (marked) {
+        style.backgroundColor = 'red';
+    }
+
+    return (
+        <li style={style}>
+            <article>
+                <h2>{props.title}</h2>
+                <div>Price: {props.price}</div>
+                <div>Year: {props.year}</div>
+                <footer>
+                    <button onClick={clickHandler}>Highlight</button>
+                    <button onClick={deleteHandler}>Delete</button>
+                    <span>Author: {props.author}</span>
+                </footer>
+            </article>
+        </li>
+    );
 };
--- FrontEnd-May-2022/ReactJS/03-components-deep-dive/src/components/BookList.js
+++ FrontEnd-May-2022/ReactJS/03-components-deep-dive/src/components/BookList.js
@@ -1,6 +1,8 @@
+import { useEffect } from "react";
 import { Book } from "./Book";
 
 export const BookList = (props) => {
+
     return (
         <ul>
             {props.books.map(book => <Book {...book} />)}
