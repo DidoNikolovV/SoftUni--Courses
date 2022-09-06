5e6f58455defe9a5904876cd4033fe431bba1db2
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-09-06 16:45:32 +0300

    Break it down into different component

--- FrontEnd-May-2022/ReactJS/04-components-workshop1/client/src/App.js
+++ FrontEnd-May-2022/ReactJS/04-components-workshop1/client/src/App.js
@@ -1,11 +1,24 @@
-import { Header } from './components/Header';
+import { Header } from './components/common/Header';
+import { Footer } from './components/common/Footer';
+import { Search } from './components/search/Search';
+import { UserSection } from './user-section/UserSection';
+import './App.css';
 
 function App() {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+
+			<main className="main">
+				<section className="card users-container">
+					<Search />
+					<UserSection />rS
+				</section>
+			</main>
+
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
